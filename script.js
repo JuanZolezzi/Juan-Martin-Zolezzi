4c78d1b2acf7c34003a8bf850d2709fe54fe9450
Juan Martin Zolezzi <juanmartinzolezzi@Laptop-de-Juan.local>
2024-10-04 14:29:33 -0300

    terminado

---
+++ script.js
@@ -0,0 +1,18 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.onclick = () => {
+    navLinks.classList.toggle ('active');
+}
+
+document.querySelector('.visit-btn').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi?tab=repositories', '_blank');
+});
+
+document.querySelector('#linkedin').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/juan-martin-zolezzi-8b76961b5/', '_blank');
+});
+
+document.querySelector('#github').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi?tab=repositories', '_blank');
+});
