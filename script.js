38a8786cac31af78e69e9aeb0de8e4a8ae07d867
Juan Martin Zolezzi <juanmartinzolezzi@Laptop-de-Juan.local>
2024-10-09 11:39:59 -0300

    terminado

--- script.js
+++ script.js
@@ -20,3 +20,185 @@ document.querySelector('#github').addEventListener('click', function() {
 document.querySelector('#whatsapp').addEventListener('click', function() {
     window.open('https://wa.me/1164484170', '_blank');
 });
+
+document.querySelector('#sch-aceros').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/SCH-Aceros/', '_blank');
+});
+
+document.querySelector('#sch-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/SCH-Aceros', '_blank');
+});
+
+document.querySelector('#qrispy').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/Qrispy/', '_blank');
+});
+
+document.querySelector('#qrispy-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/Qrispy', '_blank');
+});
+
+document.querySelector('#portfolio1').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/Proyecto-1/', '_blank');
+});
+
+document.querySelector('#portfolio1-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/Proyecto-1/', '_blank');
+});
+
+document.querySelector('#plurdiamante').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/Plurdiamante/', '_blank');
+});
+
+document.querySelector('#plurdiamante-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/Plurdiamante/', '_blank');
+});
+
+document.querySelector('#plomero').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/Plomero/', '_blank');
+});
+
+document.querySelector('#plomero-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/Plomero/', '_blank');
+});
+
+document.querySelector('#oregano').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/Oregano/', '_blank');
+});
+
+document.querySelector('#oregano-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/Oregano/', '_blank');
+});
+
+document.querySelector('#odontologia').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/Odontologia/', '_blank');
+});
+
+document.querySelector('#odontologia-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/Odontologia/', '_blank');
+});
+
+document.querySelector('#netflix').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/Proyecto-3/', '_blank');
+});
+
+document.querySelector('#netflix-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/Proyecto-3/', '_blank');
+});
+
+document.querySelector('#natura').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/Natura-Mailing/', '_blank');
+});
+
+document.querySelector('#natura-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/Natura-Mailing/', '_blank');
+});
+
+document.querySelector('#portfolio2').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/Proyecto-5/', '_blank');
+});
+
+document.querySelector('#portfolio2-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/Proyecto-5/', '_blank');
+});
+
+document.querySelector('#lenovo').addEventListener('click', function() {
+    window.open('https://test-lenovo.vercel.app/', '_blank');
+});
+
+document.querySelector('#lenovo-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/Test-Lenovo', '_blank');
+});
+
+document.querySelector('#hogar').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/electrodomesticos/', '_blank');
+});
+
+document.querySelector('#hogar-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/electrodomesticos/', '_blank');
+});
+
+document.querySelector('#emergencias').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/Urgencias-Electricas/', '_blank');
+});
+
+document.querySelector('#emergencias-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/Urgencias-Electricas/', '_blank');
+});
+
+document.querySelector('#electricista').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/electricista/', '_blank');
+});
+
+document.querySelector('#electricista-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/electricista/', '_blank');
+});
+
+document.querySelector('#deportes').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/Proyecto-2/', '_blank');
+});
+
+document.querySelector('#deportes-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/Proyecto-2/', '_blank');
+});
+
+document.querySelector('#cerrajeria').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/Cerrajeria-24hs/', '_blank');
+});
+
+document.querySelector('#cerrajeria-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/Cerrajeria-24hs/', '_blank');
+});
+
+document.querySelector('#bombas').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/bomba-rowa/', '_blank');
+});
+
+document.querySelector('#bombas-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/bomba-rowa/', '_blank');
+});
+
+document.querySelector('#mario').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/MarioBros/', '_blank');
+});
+
+document.querySelector('#mario-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/MarioBros/', '_blank');
+});
+
+document.querySelector('#ruleta').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/Ruleta/', '_blank');
+});
+
+document.querySelector('#ruleta-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/Ruleta/', '_blank');
+});
+
+document.querySelector('#serpiente').addEventListener('click', function() {
+    window.open('https://juanzolezzi.github.io/Juego-Serpiente/', '_blank');
+});
+
+document.querySelector('#serpiente-repo').addEventListener('click', function() {
+    window.open('https://github.com/JuanZolezzi/Juego-Serpiente/', '_blank');
+});
+
+
+// Mostrar/Ocultar los proyectos adicionales
+// Selección del botón y la sección de proyectos ocultos
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtener el botón y la sección de proyectos adicionales
+    var toggleButton = document.getElementById("toggleButton");
+    var moreProjects = document.getElementById("moreProjects");
+
+    // Controlar la visibilidad de los proyectos adicionales al hacer clic en el botón
+    toggleButton.addEventListener("click", function () {
+        if (moreProjects.style.display === "none" || moreProjects.style.display === "") {
+            moreProjects.style.display = "grid"; // Mostrar los proyectos
+            toggleButton.textContent = "Show Less Projects"; // Cambiar texto del botón
+        } else {
+            moreProjects.style.display = "none"; // Ocultar los proyectos
+            toggleButton.textContent = "Show More Projects"; // Cambiar texto del botón
+        }
+    });
+});
+
+
